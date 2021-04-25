d9274111b06167115bcfb8dfdb9d95b498059f46
vladimir <bloodoof1234@gmail.com>
2021-04-25 19:48:00 +0300

    deleted checkboxes

--- Project/school_log/log/forms.py
+++ Project/school_log/log/forms.py
@@ -4,7 +4,3 @@ class AddStudentForm(forms.Form):
 	name = forms.CharField()
 	birthYear = forms.DateTimeField(input_formats=['%d/%m/%Y'])
 	email = forms.EmailField()
-
-	SUBJECTS = (('1','CSS'),("2", "HTML"),("3","JS"))
-	subjects = forms.MultipleChoiceField(choices=SUBJECTS, 
-	widget=forms.CheckboxSelectMultiple())
